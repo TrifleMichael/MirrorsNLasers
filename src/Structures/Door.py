3d3b463eaa6fabf15719fbf09b174b0effe06351
Wojciech Jasiński <woj.jasinski@gmail.com>
2022-05-26 20:57:17 +0200

    door opens once

--- src/Structures/Door.py
+++ src/Structures/Door.py
@@ -1,3 +1,4 @@
+from src.LogicManager import Receiver
 from src.Sprites.LineSprite import LineSprite
 from src.Structures import Structure
 from enum import Enum
@@ -5,29 +6,31 @@ from math import pi, cos, sin
 
 
 class Direction(Enum): # TODO: problably move this to a different file
-    RIGHT = 0
-    DOWN = 1
-    LEFT = 2
-    UP = 3
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+    UP = 4
 
     def to_rotation(self):
         return self.value*pi/2
 
 
-class Door(Structure):
-    def draw(self):
-        self.sprite.draw(*self.getSurfacePoints())
-
+class Door(Structure, Receiver):
+    """A door is a structure that can be opened and closed"""
     def __init__(self, x, y, width, direction: Direction, is_open=False):
         self.x, self.y = x, y
         self.width = width
         self.is_open = is_open
         self.rotation = direction.to_rotation()
-        self.sprite = LineSprite(color=(255, 228, 196), width=15)
+        self.sprite = LineSprite(color=(139, 69, 19), width=15)
 
-    def toggle(self):
-        self.is_open = not self.is_open
-        self.rotation += pi/2 if self.is_open else -pi/2
+    def draw(self):
+        self.sprite.draw(*self.getSurfacePoints())
+
+    def trigger(self):
+        if not self.is_open:
+            self.is_open = True
+            self.rotation += pi/2
 
     def getSurfacePoints(self):
         p1 = (self.x, self.y)
