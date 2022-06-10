513662bcb32757d6fc6c93872282266bafc33ca6
Wojciech Jasiński <woj.jasinski@gmail.com>
2022-06-10 15:15:14 +0200

    window name

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,6 @@ import pygame
 from src.Settings import xResolution, yResolution
 
 pygame.init()
+pygame.display.set_caption("MirrorsNLasers")
 display = pygame.display.set_mode((xResolution, yResolution), 0, 32)
 
